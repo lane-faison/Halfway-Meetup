26242af328f51556d02a645ca5f7108c533a1ec7
Lane Faison <lane.faison@icloud.com>
2017-05-22 11:36:50 -0600

    API Key updated

--- Raddius/AppDelegate.swift
+++ Raddius/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSPlacesClient.provideAPIKey("AIzaSyBu9bK76_xLIqw0OHRXll3NbMWWWFkYgrg")
+        GMSPlacesClient.provideAPIKey("AIzaSyBpOxB0ZAVN8d6DcMwPDPl9Rfxogrte9d4")
         return true
     }
 
