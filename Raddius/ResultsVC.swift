e8c52f37ffa6c616e7c12976a1e95c9f781cd28e
Lane Faison <lane.faison@icloud.com>
2017-05-23 11:23:39 -0600

    Google Maps plots user and other user points with a red circle around the middle area

--- Raddius/ResultsVC.swift
+++ Raddius/ResultsVC.swift
@@ -7,72 +7,47 @@
 //
 
 import UIKit
-import MapKit
+import GoogleMaps
 
-class ResultsVC: UIViewController, MKMapViewDelegate {
-    
-    @IBOutlet weak var mapView: MKMapView!
+class ResultsVC: UIViewController {
     
     var positions: Positions!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mapView.delegate = self
-        
         let midLatitude = (positions.thisUsersLocation.latitude+positions.otherUsersLocation.latitude)/2
         let midLongitude = (positions.thisUsersLocation.longitude+positions.otherUsersLocation.longitude)/2
         let midpoint = CLLocationCoordinate2D(latitude: midLatitude, longitude: midLongitude)
         
-        let userAnnotation = AnnotationPoint(coordinate: positions.thisUsersLocation)
-        let otherAnnotation = AnnotationPoint(coordinate: positions.otherUsersLocation)
         
         let locationUser = CLLocation(latitude: positions.thisUsersLocation.latitude, longitude: positions.thisUsersLocation.longitude)
         let locationOther = CLLocation(latitude: positions.otherUsersLocation.latitude, longitude: positions.otherUsersLocation.longitude)
         let distance: CLLocationDistance = locationUser.distance(from: locationOther)
+//        let distanceSpan: CLLocationDegrees = 2000 + distance
+        print("DISTANCE: \(distance)")
+        let camera = GMSCameraPosition.camera(withLatitude: midLatitude, longitude: midLongitude, zoom:13)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
         
-        let distanceSpan: CLLocationDegrees = 2000 + distance
+        // Creates a marker for user's location on the map.
+        let userMarker = GMSMarker()
+        userMarker.position = positions.thisUsersLocation
+        userMarker.title = "Your Location"
+        userMarker.icon = GMSMarker.markerImage(with: .green)
+        userMarker.map = mapView
+        // Creates a marker for other person's location on the map.
+        let otherMarker = GMSMarker()
+        otherMarker.position = positions.otherUsersLocation
+        otherMarker.title = "Their Location"
+        otherMarker.icon = GMSMarker.markerImage(with: .red)
+        otherMarker.map = mapView
         
-        let circle = MKCircle(center: midpoint, radius: distance/6)
-        
-        let request = MKLocalSearchRequest()
-        
-        request.naturalLanguageQuery = "food"
-        
-        mapView.setRegion(MKCoordinateRegionMakeWithDistance(midpoint, distanceSpan, distanceSpan), animated: true)
-        mapView.addAnnotation(userAnnotation)
-        mapView.addAnnotation(otherAnnotation)
-        mapView.addOverlays([circle])
-        request.region = mapView.region
-        let search = MKLocalSearch(request: request)
-        search.start(completionHandler: {(result,error) in
-            for placemark in (result?.mapItems)! {
-                if(error == nil) {
-                    
-                    let annotation = MKPointAnnotation()
-                    annotation.coordinate = CLLocationCoordinate2DMake(placemark.placemark.coordinate.latitude, placemark.placemark.coordinate.longitude)
-                    annotation.title = placemark.placemark.name
-                    annotation.subtitle = placemark.placemark.title
-                    self.mapView.addAnnotation(annotation)
-                    
-                }
-                else
-                {
-                    print(error ?? 0)
-                }
-            }
-        })
+        let circle = GMSCircle(position: midpoint, radius: distance/5)
+        circle.strokeColor = UIColor.red
+        circle.strokeWidth = 2
+        circle.map = mapView
     }
-    
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        if overlay.isKind(of: MKCircle.self){
-            let circleRenderer = MKCircleRenderer(overlay: overlay)
-            circleRenderer.fillColor = UIColor.blue.withAlphaComponent(0.1)
-            circleRenderer.strokeColor = UIColor.blue
-            circleRenderer.lineWidth = 1
-            return circleRenderer
-        }
-        return MKOverlayRenderer(overlay: overlay)
-    }
+
     
 }
